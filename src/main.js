bcdd9732b7cfdb4270cf32e7cae01b277138e965
Vitor Miranda <vitortcmout@gmail.com>
2020-01-26 16:25:53 -0300

    Aplicação de lista simples com ES6+

--- src/main.js
+++ src/main.js
@@ -1,15 +1,57 @@
-import axios from 'axios';
+class App{
+    constructor(){
+        this.repositories = [];
 
-class Api{
-    static async getUserInfo(username) {
-        try {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
-        console.log(response);
-    }catch(err){
-        console.warn('Erro na Api');
+        this.registerHandlers();
     }
+    registerHandlers(){
+        this.formEl.onsubmit = () => this.addRepository(event);
+    }
+
+    addRepository(event){
+        event.preventDefault();
+        //preventDefault previne que o form recarregue a página como envio do post
+        this.repositories.push({
+            name:'Vitor',
+            description:'Dev',
+            avatar_url:'https://avatars1.githubusercontent.com/u/37985642?s=460&v=4',
+            html_url:'https://github.com/vitortcmiranda/CursoEs6'
+        });
+        this.render();
+        console.log(this.repositories)
+    }
+    render(){
+        this.listEl.innerHTML='';
+
+        this.repositories.forEach(repo =>{
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src',repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createElement(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description))
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','_blank');
+            linkEl.setAttribute('href',html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+
+        });
     }
 }
 
-Api.getUserInfo('vitortcmiranda');
+new App();
