5a77eb868e36963457bb20c48e702f5b5865d52a
Vitor Miranda <vitortcmout@gmail.com>
2020-01-26 15:56:05 -0300

    Trabalhando com async awayt

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import ClasseUsuario,{idade as IdadeUsuario} from './functions';
+import axios from 'axios';
 
-console.log(ClasseUsuario.info);
-console.log(IdadeUsuario);
+class Api{
+    static async getUserInfo(username) {
+        try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        console.log(response);
+    }catch(err){
+        console.warn('Erro na Api');
+    }
+    }
+}
+
+Api.getUserInfo('vitortcmiranda');
